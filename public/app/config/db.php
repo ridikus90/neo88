9d2c3138c00e0cefe159247f5631f56d9be9addd
Tolik <vurrayon21@gmail.com>
2015-07-10 11:02:31 +0300

    initial project version

---
+++ public/app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=neo88',
+    'username' => 'root',
+    'password' => '6545',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
